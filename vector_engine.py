ec534a95b517f119de21fa7b4ac8de500d06b64a
Thorge-L <thorge.lerche05@gmail.com>
2026-02-15 20:10:32 +0100

    Fix cosine similarity and improve code quality

--- vector_engine.py
+++ vector_engine.py
@@ -1,16 +1,21 @@
-# Day 3 Einführung in die Klassen
+# Einführung in die Klassen
 
 import math
 
 class VectorEngine:
+    @staticmethod
+    def _check_dimensions(v1, v2):
+    if len(v1) != len(v2):
+            raise ValueError("Vectors must be of the same length")
+
+
 
     @staticmethod  # scalar product
-    def scalar_product(vector_a, vector_b):
-        if len(vector_a) != len(vector_b):
-            raise ValueError("Vectors must be of the same length")
+    def scalar_product(v1, v2):
+        VectorEngine._check_dimensions(v1, v2)
         
         product = 0
-        for a, b in zip(vector_a, vector_b):
+        for a, b in zip(v1, v2):
             product += a * b
 
         return product
@@ -21,18 +26,22 @@ class VectorEngine:
         return math.sqrt(sum_of_squares) 
 
     @staticmethod  # Manhattan distance L1 norm
-    def d(a, b):
+    def d(v1, v2):
+        VectorEngine._check_dimensions(v1, v2)
         distance = 0
-        for coordinate1,coordinate2 in zip(a, b):
+        for coordinate1,coordinate2 in zip(v1, v2):
             distance += abs(coordinate1 - coordinate2)
         return distance
     
     @staticmethod  # Cosine similarity
-    def cosine_sim(a: list[float], b: list[float]):
-        if len(a) != len(b):
-            raise ValueError("Vectors must be of same length")
-        if VectorEngine.euclidean_norm(a) == 0.0 or VectorEngine.euclidean_norm(b) == 0.0:
+    def cosine_sim(v1: list[float], v2: list[float]):
+        VectorEngine._check_dimensions(v1, v2)
+
+        norm_v1 = VectorEngine.euclidean_norm(v1)
+        norm_v2 = VectorEngine.euclidean_norm(v2)
+
+        if norm_v1 or norm_v2 < 1e-10:
             return 0.0
-        else:
-            similarity = (VectorEngine.scalar_product(a, b)) / (VectorEngine.euclidean_norm(a) * VectorEngine.euclidean_norm(b))
+        
+        similarity = (VectorEngine.scalar_product(v1, v2)) / (VectorEngine.euclidean_norm(v1) * VectorEngine.euclidean_norm(v2))
         return similarity
